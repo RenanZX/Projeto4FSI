95a5eae4e29609aa0693f42ce515670aad58f5dc
Renan Godoi <renangodoimedeiros@gmail.com>
2018-11-16 01:58:20 -0200

    projeto

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Projeto4FSI
+# Projeto4 - Fundamentos em Sistemas Inteligentes
+O projeto consiste em treinar e testar dois modelos de Redes Neuronais Artificiais, uma MLP(Perceptron Multicamadas), e uma FBR, em uma base de dados indicativos de spam.
+
+## Requisitos
+1. É necessário instalar o R e configurá-lo caso queira executar este programa
+- [Versão linux](http://www.jason-french.com/blog/2013/03/11/installing-r-in-linux/)
+- [Versão Windows](https://cran.r-project.org/bin/windows/base/)
+
+2. Instale os pacotes dos modelos mlp e fbr do R utilizando o comando ```install.packages("RSNSS")```
+
+## Instruções de Execução
+
+1. Vá até a pasta do projeto
+
+2. Digite ```Rscript Projeto4.r```
+
+3. O programa pedirá para você selecionar o arquivo corresponde ao banco de dados,vá até a pasta raiz do projeto e selecione o arquivo ```spam_data.mat```
+
+4. Por fim o programa executará e mostrará os resultados após serem processados.
+
+link do github: https://github.com/RenanZX/Projeto4FSI
---
+++ projeto4.r
@@ -0,0 +1,69 @@
+#SVM
+library(RSNNS)
+library(ggplot2)
+library(R.matlab)
+
+randdata = function(data){
+  dP <- cbind(data$P.train,data$P.test)
+  dT <- cbind(data$T.train,data$T.test)
+  kdat <- list()
+  dPv <- sample(ncol(dP), 0.9*ncol(dP), replace = TRUE)
+  kdat$P.train <- dP[,dPv]
+  kdat$P.test <- dP[,-dPv]
+  kdat$T.train <- t(as.matrix(dT[,dPv]))
+  kdat$T.test <- t(as.matrix(dT[,-dPv]))
+  kdat
+}
+
+leitor <- list(
+  readfile = function(){
+    data <- readMat(file.choose())
+    data
+  }
+)
+
+processor <- list(
+  ProcessarFBR = function(dados,sz,maxtit){
+    yt <- t(as.matrix(dados$T.train))
+    xt <- t(as.matrix(dados$P.train))
+    
+    modelo <- rbf(x = xt, y = yt, size = sz, maxit = maxtit, linOut = TRUE)
+    predteste <- sign(predict(modelo, t(as.matrix(dados$P.test))))
+    
+    mean(predteste == t(dados$T.test))
+  },
+  ProcessarMLP = function(dados,sz,maxtit){
+    #https://rdrr.io/cran/RSNNS/man/mlp.html
+    
+    yt <- t(as.matrix(dados$T.train))
+    xt <- t(as.matrix(dados$P.train))
+    
+    modelo <- mlp(x = xt, y = yt, size = sz, maxit = maxtit, linOut = TRUE)
+    predteste <- sign(predict(modelo, t(as.matrix(dados$P.test))))
+    
+    mean(predteste == t(dados$T.test))
+  }
+)
+
+data = leitor$readfile()
+
+cvalues <- 0:10
+
+for (i in 0:11){
+  rdata <- randdata(data)
+  cvalues[i] = processor$ProcessarFBR(rdata,40,1000)*100
+}
+
+cvaluesmlp <- 0:10
+
+for (i in 0:11){
+  rdata <- randdata(data)
+  cvaluesmlp[i] = processor$ProcessarMLP(rdata,40,1000)*100
+}
+
+plot(cvalues, type="o", col="blue",xlab = "modelos",ylab = "Taxa de acerto %", main = "Radial Basis Function x Multi Layer Perceptron",ylim=c(0,80))
+
+lines(cvaluesmlp, type="o", pch=22, lty=2, col="red")
+
+legend(c(0,20), legend=c("RBF","MLP"),
+       col=c("blue","red"), lty=1:2, cex=0.5)
